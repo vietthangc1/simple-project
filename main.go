48cef1342ed0b06ebb8fbda6e9a21d11dcb87ed6
thangpham4 <thang.pham4@tiki.vn>
2023-04-21 08:20:54 +0700

    add hello path

--- main.go
+++ main.go
@@ -1,31 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 type ReturnMessage struct {
-	Code uint32 `json:"code"`
+	Code    uint32 `json:"code"`
 	Message string `json:"message"`
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/", func (c *gin.Context) {
+	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusFound, ReturnMessage{
-			Code: 200,
+			Code:    200,
 			Message: "This is Homepage!",
 		})
 	})
-	router.GET("/home", func (c *gin.Context) {
+	router.GET("/home", func(c *gin.Context) {
 		c.JSON(http.StatusFound, ReturnMessage{
-			Code: 200,
+			Code:    200,
 			Message: "Welcome home!",
 		})
 	})
+	router.GET("/hello/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.JSON(http.StatusFound, ReturnMessage{
+			Code:    200,
+			Message: fmt.Sprintf("Hello %s",name),
+		})
+	})
 
-	router.Run(":80")
+	router.Run(":4001")
 
 }
